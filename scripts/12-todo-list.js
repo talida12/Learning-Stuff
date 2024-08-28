f2e57d00bd80036f53127ea9673c82d2af5704f6
talida12 <talidadirnu@gmail.com>
2024-08-28 20:36:22 +0300

    finished chapter 12: advanced functions

--- scripts/12-todo-list.js
+++ scripts/12-todo-list.js
@@ -10,21 +10,30 @@ renderTodoList();
 
 function renderTodoList() { 
   let todoListHTML = '';
-  for (let i = 0; i < todoList.length; i++) { 
-    const todoObject = todoList[i];
-    const { name, dueDate} = todoObject;
+  
+  todoList.forEach((todoObject, index) => {
+    const { name, dueDate } = todoObject;
     const html = `
     <div>${name}</div>
     <div>${dueDate}</div>
-    <button onclick="
-      todoList.splice(${i}, 1);
-      renderTodoList();
-    " class="delete-todo-button">Delete</button>  
+    <button class="delete-todo-button js-delete-todo-button">Delete</button>  
     `;
     todoListHTML += html;
-  }
+  });
   document.querySelector('.js-todo-list').innerHTML = todoListHTML;
+
+  document.querySelectorAll('.js-delete-todo-button')
+    .forEach((deleteButton, index) => {
+      deleteButton.addEventListener('click', () => {
+        todoList.splice(index, 1);
+        renderTodoList();
+      });
+    });
 }
+
+document.querySelector('.js-add-todo-button').addEventListener('click', () => { 
+  addTodo();
+});
 
 function addTodo() {
   const inputElement = document.querySelector('.js-name-input');
